45271970559e99dc6c9c2084890a822779a76196
Isuru Fernando <isuruf@gmail.com>
2024-03-15 14:12:22 -0500

    update to 6

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-sed -i 's@set(CMAKE_CXX_FLAGS "-std=c++11 ")@@g' CMakeLists.txt
-
 mkdir build
 cd build
 
-cmake \
+cmake ${CMAKE_ARGS} \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_LIBDIR=lib \
